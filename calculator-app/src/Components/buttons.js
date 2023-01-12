a261039d55cb67f98765eea95dba082b43fead1d
sngoyo <salim@kali>
2023-01-12 13:24:14 +0300

    changes in putting all buttons in one div container

--- calculator-app/src/Components/buttons.js
+++ calculator-app/src/Components/buttons.js
@@ -10,39 +10,25 @@ function Button () {
         <div >
         <calculator />
         <div className="buttons-container">
-        <div className="opsAndClearButton">
-
-        <div className="Operators-container">
-        <button id="clear">AC</button>
-
+           <button id="clear" className="span-two">AC</button>
            <button id="divide" className="ops-button">/</button>
            <button id="multiply" className="ops-button">x</button>
+           <button id="seven">7</button>
+           <button id="eight">8</button>
+           <button id="nine">9</button>
            <button id="subtract" className="ops-button">-</button>
+           <button id="four">4</button>
+           <button id="five">5</button>
+           <button id="six">6</button>
            <button id="add" className="ops-button">+</button>
-
-           <button id="equals">=</button>
-          <div className="numbersButtons">
-             <button id="seven">7</button>
-             <button id="eight">8</button>
-             <button id="nine">9</button>
-             <button id="four">4</button>
-             <button id="five">5</button>
-             <button id="six">6</button>
-             <button id="one">1</button>
-             <button id="two">2</button>
-             <button id="three">3</button>
-             <button id="zero">0</button>
-             <button id="decimal">.</button>
+           <button id="one">1</button>
+           <button id="two">2</button>
+           <button id="three">3</button>
+           <button id="equals" className="span-two-vertically">=</button>
+           <button id="zero" className="span-two">0</button>
+           <button id="decimal" >.</button>
         </div>
         </div>
-        </div>
-
-
-
-        </div>
-
-        </div>
-
     );
 }
 
